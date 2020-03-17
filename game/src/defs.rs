797a49be51438c09409dca88058d367db582cdfb
travistrue2008 <travis.true08@gmail.com>
2020-03-17 02:42:39 -0700

    Fixed missing/broken data for configs. Moved all definition logic to its own module. Began loading data.

---
+++ game/src/defs.rs
@@ -0,0 +1,159 @@
+use std::fs;
+use serde::{Deserialize};
+
+#[derive(Debug, Deserialize)]
+pub enum ArmorKind {
+	Shield,
+	Helm,
+	Armor,
+	Robe,
+	Ring,
+	Gauntlets,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum WeaponKind {
+	Sword,
+	DarkSword,
+	HolySword,
+	Hammer,
+	Katana,
+	Knife,
+	Axe,
+	Bow,
+	Arrow,
+	Boomerang,
+	Claw,
+	Harp,
+	Rod,
+	Staff,
+	Spear,
+	Whip,
+}
+
+#[derive(Debug, Deserialize)]
+pub enum Handedness {
+	Left,
+	Right,
+	Both,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Item {
+	id: i32,
+	price: i32,
+	name: String,
+	description: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Armor {
+	id: i32,
+	price: i32,
+	defense: i32,
+	#[serde(alias = "magicDefense")]
+	magic_defense: i32,
+	name: String,
+	#[serde(alias = "type")]
+	kind: ArmorKind,
+	modifiers: Vec<i32>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Weapon {
+	id: i32,
+  price: i32,
+	attack: i32,
+	#[serde(alias = "hitRate")]
+	hit_rate: i32,
+	name: String,
+	#[serde(alias = "type")]
+  kind: WeaponKind,
+  modifiers: Vec<i32>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct EnemyDrop {
+	#[serde(alias = "itemId")]
+	item_id: i32,
+	quantity: i32,
+	rate: u8,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Enemy {
+	id: i32,
+	hp: i32,
+	strength: i32,
+	defense: i32,
+	magic: i32,
+	#[serde(alias = "magicDefense")]
+	magic_defense: i32,
+	gil: i32,
+	exp: i32,
+	name: String,
+	treasure: Vec<EnemyDrop>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Command {
+	name: String,
+	description: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct PartyMember {
+	id: i32,
+	name: String,
+	class: String,
+	handedness: Handedness,
+	#[serde(alias = "weaponTypes")]
+	weapon_kinds: Vec<WeaponKind>,
+	commands: Vec<Command>,
+}
+
+pub struct Definitions {
+	items: Vec<Item>,
+	armor: Vec<Armor>,
+	weapons: Vec<Weapon>,
+	enemies: Vec<Enemy>,
+	members: Vec<PartyMember>,
+}
+
+impl Definitions {
+	pub fn items(&self) -> &Vec<Item> {
+		&self.items
+	}
+
+	pub fn armor(&self) -> &Vec<Armor> {
+		&self.armor
+	}
+
+	pub fn weapons(&self) -> &Vec<Weapon> {
+		&self.weapons
+	}
+
+	pub fn enemies(&self) -> &Vec<Enemy> {
+		&self.enemies
+	}
+
+	pub fn members(&self) -> &Vec<PartyMember> {
+		&self.members
+	}
+}
+
+pub fn load() -> Definitions {
+	let item_contents = fs::read_to_string("./resources/items.json").unwrap();
+	let armor_contents = fs::read_to_string("./resources/armor.json").unwrap();
+	let weapon_contents = fs::read_to_string("./resources/weapons.json").unwrap();
+	let enemy_contents = fs::read_to_string("./resources/enemies.json").unwrap();
+	let member_contents = fs::read_to_string("./resources/party.json").unwrap();
+
+	Definitions {
+		items: serde_json::from_str(&item_contents).unwrap(),
+		armor: serde_json::from_str(&armor_contents).unwrap(),
+		weapons: serde_json::from_str(&weapon_contents).unwrap(),
+		enemies: serde_json::from_str(&enemy_contents).unwrap(),
+		members: serde_json::from_str(&member_contents).unwrap(),
+	}
+}
--- game/src/main.rs
+++ game/src/main.rs
@@ -1,52 +1,9 @@
-use std::fs;
-use serde::{Deserialize};
-use serde_json::{Value};
-
-const FILENAME: &str = "./resources/enemies.json";
-
-#[derive(Debug, Deserialize)]
-struct EnemyDrop {
-	item_id: i32,
-	rate: u8,
-}
-
-#[derive(Debug, Deserialize)]
-struct Enemy {
-	id: i32,
-	name: String,
-	hp: i32,
-	strength: i32,
-	defense: i32,
-	magic: i32,
-	#[serde(alias = "magicDefense")]
-	magic_defense: i32,
-	gil: i32,
-	exp: i32,
-	treasure: Vec<EnemyDrop>,
-}
-
-impl Enemy {
-	fn make() -> Enemy {
-		Enemy {
-			id: 0,
-			name: "",
-			hp: 0,
-			strength: 0,
-			defense: 0,
-			magic: 0,
-			magic_defense: 0,
-			gil: 0,
-			exp: 0,
-			treasure: Vec(),
-		}
-	}
-}
-
+mod defs;
 
 fn main() {
-	let contents = fs::read_to_string(FILENAME).expect("Unable to read file");
-	let v: Vec<Enemy> = serde_json::from_str(&contents)
-		.expect("Cannot deserialize data");
+	let definitions: defs::Definitions = defs::load();
 
-	println!("v: {:?}", v);
+	for enemy in definitions.enemies() {
+		println!("enemies: {:?}", enemy);
+	}
 }
