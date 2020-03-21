da914ea68c3b31b8ed22bc8786ac0e4d277fba4b
travistrue2008 <travis.true08@gmail.com>
2020-03-21 15:20:53 -0700

    Converted lzs-extractor to pre-load all data into memory, then map

--- lzs-extractor/src/main.rs
+++ lzs-extractor/src/main.rs
@@ -1,13 +1,12 @@
 use byteorder::{ByteOrder, LittleEndian};
+use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::BufReader;
-use std::io::SeekFrom;
 use std::str;
 
 struct FileEntry {
-	offset: u32,
-	size: u32,
+	offset: usize,
+	size: usize,
 	name: String,
 }
 
@@ -16,54 +15,61 @@ struct Header {
 	entries: Vec<FileEntry>,
 }
 
-fn read_u16(reader: &mut BufReader<&File>) -> u16 {
-	let mut buffer = [0; 2];
+fn get_slice<'a>(buffer: &'a [u8], offset: &mut usize, length: usize) -> &'a [u8] {
+	let start_index = *offset as usize;
+	let end_index = start_index + length;
 
-	reader.read(&mut buffer).unwrap();
-	LittleEndian::read_u16(&buffer)
+	*offset += length;
+	&buffer[start_index..end_index]
 }
 
-fn read_u32(reader: &mut BufReader<&File>) -> u32 {
-	let mut buffer = [0; 4];
+fn read_u16(buffer: &[u8], offset: &mut usize) -> u16 {
+	let slice = get_slice(buffer, offset, 2);
 
-	reader.read(&mut buffer).unwrap();
-	LittleEndian::read_u32(&buffer)
+	LittleEndian::read_u16(slice)
 }
 
-fn read_str(reader: &mut BufReader<&File>) -> String {
-	let mut buffer = [0; 48];
+fn read_usize(buffer: &[u8], offset: &mut usize) -> usize {
+	let slice = get_slice(buffer, offset, 4);
 
-	reader.read(&mut buffer).unwrap();
+	LittleEndian::read_u32(slice) as usize
+}
 
-	for i in 0..buffer.len() {
-		if buffer[i] == 255 {
-			buffer[i] = 0;
-		}
+fn read_str(buffer: &[u8], offset: &mut usize) -> String {
+	let start_index = *offset as usize;
+	let end_index = start_index + 48;
+	let slice = &buffer[start_index..end_index];
+	let mut copy_buffer = [0u8; 48];
+
+	for (i, value) in slice.iter().enumerate() {
+		copy_buffer[i] = if *value >= 32 && *value < 127 { *value } else { 32 };
 	}
 
-	let result = str::from_utf8(&buffer).unwrap();
+	let result = str::from_utf8(&copy_buffer).unwrap().trim();
+
+	*offset += 48;
 	String::from(result)
 }
 
-fn read_header(reader: &mut BufReader<&File>) -> Header {
-	let count = read_u16(reader);
+fn read_header(buffer: &[u8], offset: &mut usize) -> Header {
 	let mut entries = Vec::new();
+	let count = read_u16(buffer, offset);
 
 	for i in 0..(count as u32) {
-		if i > 0 {
-			read_u16(reader);
-		}
+		*offset += if i > 0 { 4 } else { 2 };
 
-		read_u16(reader);
+		let file_offset = read_usize(buffer, offset);
+		let size = read_usize(buffer, offset);
+		let file_num = read_usize(buffer, offset);
+		let name = read_str(buffer, offset);
 
-		let offset = read_u32(reader);
-		let size = read_u32(reader);
-		let file_num = read_u32(reader);
-		let name = read_str(reader);
+		entries.push(FileEntry {
+			offset: file_offset,
+			size,
+			name,
+		});
 
-		entries.push(FileEntry { offset, size, name });
-
-		if file_num != i {
+		if file_num != i as usize {
 			println!("WARN: invalid file num: {} ({})", file_num, i);
 		}
 	}
@@ -71,22 +77,24 @@ fn read_header(reader: &mut BufReader<&File>) -> Header {
 	Header { count, entries }
 }
 
-fn write_file (reader: &mut BufReader<&File>) {
-}
-
 fn main() {
+	let mut offset: usize = 0;
+	let mut buffer = Vec::new();
 	let mut file = File::open("./assets/CN_ancient_waterway.lzs").unwrap();
-	let mut reader = BufReader::new(&file);
-	let header = read_header(&mut reader);
+	
+	file.read_to_end(&mut buffer);
 
+	let header = read_header(&buffer, &mut offset);
 	for (i, entry) in header.entries.iter().enumerate() {
-		println!("{}: {}", i, entry.name);
+		fs::create_dir_all("./assets/CN_ancient_waterway/");
 
-		file.seek(SeekFrom::Start(entry.offset as u64)).unwrap();
+		let path = format!("./assets/CN_ancient_waterway/{}", entry.name);
+		let mut file = File::create(path).unwrap();
 
-		let mut buffer = Vec::with_capacity(entry.size as usize);
-		let mut reader = BufReader::new(&file);
-		let pos = reader.read(&mut buffer).unwrap();
-		println!("pos: {}", pos);
+		let start_offset = entry.offset;
+		let end_offset = start_offset + entry.size as usize;
+		let slice = &buffer[start_offset..end_offset];
+
+		file.write(slice);
 	}
 }
