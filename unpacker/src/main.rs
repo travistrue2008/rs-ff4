761d7da36e98ca62ece3b6645203dc0f6f0bec7a
travistrue2008 <travis.true08@gmail.com>
2020-04-17 23:10:08 -0700

    Added CLI arguments. Fixed decoding bug for LZSS images.

--- unpacker/src/main.rs
+++ unpacker/src/main.rs
@@ -2,36 +2,85 @@ mod common;
 mod error;
 mod lzss;
 mod metadata;
-mod unpacker;
+mod extractor;
 
-use clap::{Arg, App};
-use std::path::{Path, PathBuf};
+use crate::extractor::{bin, lzs};
 
-fn main() {
-    let matches = App::new("Final Fantasy IV - Archive Unpacker")
-        .version("1.0")
-        .author("Travis J True")
-        .about("Unpacks files from the .BIN files found in the PSP ISO of FF4")
-        .arg(Arg::with_name("path")
-            .short("p")
-            .long("path")
-            .help("Path to PAC0.BIN and PAC1.BIN files")
-            .takes_value(true))
-        .arg(Arg::with_name("output")
-            .short("o")
-            .long("output")
-            .help("Output path")
-            .takes_value(true))
-        .get_matches();
+use clap::{Arg, ArgMatches, App, SubCommand};
+use std::path::PathBuf;
 
-    let input_path = Path::new(matches.value_of("path").unwrap_or("./"));
-    let output_path = match matches.value_of("output") {
+fn get_input_path(matches: &ArgMatches) -> PathBuf {
+    PathBuf::from(matches.value_of("path").unwrap_or("./"))
+}
+
+fn get_output_path(matches: &ArgMatches) -> PathBuf {
+    let input_path = get_input_path(matches);
+
+    match matches.value_of("output") {
         Some(v) => PathBuf::from(v),
         None => PathBuf::from(input_path),
-    };
+    }
+}
 
+fn process_bin(matches: &ArgMatches) {
+    let recursive = matches.is_present("recursive");
+    let input_path = get_input_path(matches);
+    let output_path = get_output_path(matches).join("output");
     let meta_path = input_path.join("PAC0.BIN");
     let archive_path = input_path.join("PAC1.BIN");
 
-    unpacker::unpack(meta_path, archive_path, output_path).unwrap();
+    bin::process(&meta_path, &archive_path, &output_path, recursive).unwrap();
+}
+
+fn process_lzs(matches: &ArgMatches) {
+    let recursive = matches.is_present("recursive");
+    let input_path = get_input_path(matches);
+    let output_path = get_output_path(matches);
+
+    lzs::process_file(&input_path, &output_path, recursive).unwrap();
+}
+
+fn main() {
+    let app = App::new("Final Fantasy IV - Archive unpacker")
+        .version("1.0")
+        .author("Travis J True")
+        .about("Unpacks files from the .BIN files found in the PSP ISO of FF4")
+        .subcommand(SubCommand::with_name("bin")
+            .arg(Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .help("Path to PAC0.BIN and PAC1.BIN files")
+                .takes_value(true))
+            .arg(Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .help("Output path")
+                .takes_value(true))
+            .arg(Arg::with_name("recursive")
+                .short("r")
+                .long("recursive")
+                .help("Recursively decompress subsequent lzs files")))
+        .subcommand(SubCommand::with_name("lzs")
+            .arg(Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .help("Path to .lzs file")
+                .takes_value(true))
+            .arg(Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .help("Output path")
+                .takes_value(true))
+            .arg(Arg::with_name("recursive")
+                .short("r")
+                .long("recursive")
+                .help("Recursively decompress subsequent lzs files")));
+
+    let matches = app.get_matches();
+
+    match matches.subcommand() {
+        ("bin", Some(m)) => process_bin(&m),
+        ("lzs", Some(m)) => process_lzs(&m),
+        _ => println!("{}", matches.usage()),
+    };
 }
